7b805f879c965bafa3aaa5178b4b64e4f3aba77a
dr3amf4ll <dr3amf4ll@gmail.com>
2017-02-04 00:16:37 +0200

    Added read/write current count to file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var fs = require('fs');
 var port = process.env.PORT || 5000;
 
 app.get('/', function(req, res){
@@ -9,12 +10,18 @@ app.get('/', function(req, res){
 
 var count = 0;
 
+fs.readFile('count', 'utf8', function(err, contents) {
+    count = +contents;
+});
+
 io.on('connection', function(socket){
 
   io.emit("loaded", count);
 
   socket.on('button click', function(){
 	count += 1;
+	
+	fs.writeFile('count', count);	
 	io.emit('button click', count);
   });
 });
